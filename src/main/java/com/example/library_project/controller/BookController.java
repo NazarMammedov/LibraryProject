d89f3549af0b5027b86d20299e2346afde924b14
= <=>
2025-03-30 14:20:50 +0200

    Initial commit

--- src/main/java/com/example/library_project/controller/BookController.java
+++ src/main/java/com/example/library_project/controller/BookController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
-import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
